6c49ab2e3b88e0a545adc2b8e75ca2e494987b0d
koo6132 <146702312+koo6132@users.noreply.github.com>
2023-11-10 10:07:53 +0900

    koo6132
    new

--- Assets/Script/Source.cs
+++ Assets/Script/Source.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Source : MonoBehaviour
 {
+    public static Source instance;
     // Start is called before the first frame update
     public AudioSource mse;
     public AudioSource cat;
+
+   void Awake()
+    { 
+
+    }
     public void SetMusicVolume(float volume) 
     {
         mse.volume = volume;
--- Assets/Test/PlayerMove.cs
+++ Assets/Test/PlayerMove.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class PlayerMove : MonoBehaviour
 {
+   
+    public static PlayerMove instance;
     Rigidbody rig;
     private Transform myT;
     private Vector3 mv = Vector3.zero;
-    float speed = 5.0f;
+    public float speed;
     float X;
     float Z;
     bool Isjump;
     public float jumpF;
-
     private Vector3 mymouce = Vector3.up;
-    float mspeed;
+    public float mspeed ;
     float mouseR;
 
     private void Awake()
@@ -24,37 +27,28 @@ public class PlayerMove : MonoBehaviour
         Isjump = false;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Space) && !Isjump)
         {
             rig.AddForce(new Vector3(0, jumpF, 0), ForceMode.Impulse);
             Isjump = true;
         }
-        mouseR = Input.GetAxis("Mouse X");
+       // mouseR = Input.GetAxis("Mouse X");
         Z = Input.GetAxis("Vertical");
         X = Input.GetAxis("Horizontal");
         mv.x = X;
         mv.z = Z;
-        myT.Translate(mv * speed * Time.deltaTime);
-        myT.Rotate(mymouce * mspeed * Time.deltaTime * mouseR);
+        myT.Translate(mv * speed * Time.fixedDeltaTime );
+       // myT.Rotate(mymouce * mspeed * Time.deltaTime * mouseR);
 
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Floor")
-            { Isjump = false; }
-
-        if (collision.gameObject.tag == "Kill")
-        {
-            //SceneManager.LoadScene("Level" + (manager.Level).ToString());
-        }
-
+        if (collision.gameObject.name == "Floor")
+            Isjump = false;
 
     }
-<<<<<<< Updated upstream:Assets/Test/PlayerMove.cs
-=======
-
->>>>>>> Stashed changes:Assets/Script/PlayerMove.cs
+    
 }
