0d21bc236db97d0c8fd598d22d6949bc2b13fed9
HYJJJ <146905014+evadvdfc@users.noreply.github.com>
2023-11-14 17:05:15 +0900

    용준 1차 다시

--- Assets/PBR_Speed_Trap/3dobjects/Materials/radarfalle.cs
+++ Assets/PBR_Speed_Trap/3dobjects/Materials/radarfalle.cs
@@ -7,26 +7,16 @@ using UnityEngine;
 public class radarfalle : MonoBehaviour
 {
     public MeshRenderer bpObj;
-    public Material zememat;
-    public Material redmat;
-    public Material bulemat;
+    public Material redMat;
+    public Material blueMat;
     public button tr;
    
-<<<<<<< Updated upstream
-   
-    // Start is called before the first frame update
-    void Start()
-    {
-=======
     
     // Start is called before the first frame update
     void Start()
     {
         tr.onTriggerButton += ChangeMatToNormal;
->>>>>>> Stashed changes
         
-        tr.onTriggerButton += ChangeMatToNormal;
-
 
 
 
@@ -37,14 +27,8 @@ public class radarfalle : MonoBehaviour
     public void ChangeMatToNormal()
     {
         Material[] mat = this.GetComponent<Renderer>().materials;
-<<<<<<< Updated upstream
-        mat[1] = bulemat;
-        this.GetComponent<Renderer>().materials = mat; 
-        
-=======
         mat[1] = blueMat;
         this.GetComponent<Renderer>().materials = mat;
->>>>>>> Stashed changes
         Debug.Log("æ»≥Á«œººø‰");
         
     }
